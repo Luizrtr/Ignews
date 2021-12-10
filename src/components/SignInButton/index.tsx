adb4545b9968371e532ab0e7ba43221c9c868ff6
Luiz Eduardo <luiz.lr.2010@gmail.com>
2021-12-10 15:55:23 -0300

    Add faunaDB e configurando

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,27 +1,27 @@
 import { FaGithub } from "react-icons/fa";
 import { FiX } from "react-icons/fi";
-import { signIn, useSession } from "next-auth/client";
+import { signIn, signOut, useSession } from "next-auth/client";
 
 import styles from "./styles.module.scss";
 
 export function SignInButton() {
   const [session] = useSession();
 
-  console.log(session);
-
   return session ? (
-    <button type="button" className={styles.signInButton}>
+    <button
+      type="button"
+      className={styles.signInButton}
+      onClick={() => signOut()}
+    >
       <FaGithub color="#04d361" />
-      Luiz Eduardo
+      {session.user.name}
       <FiX color="3737380" className={styles.closeIcon} />
     </button>
   ) : (
     <button
       type="button"
       className={styles.signInButton}
-      onClick={() => {
-        signIn("github");
-      }}
+      onClick={() => signIn("github")}
     >
       <FaGithub color="#eba417" />
       Sing in with Github
