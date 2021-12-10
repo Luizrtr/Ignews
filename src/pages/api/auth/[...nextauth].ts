73b837b08513cdc13b87a872f2f3e9fbe008efaa
Luiz Eduardo <luiz.lr.2010@gmail.com>
2021-12-10 16:59:33 -0300

    up

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,9 @@
+import { query as q } from "faunadb";
+
 import NextAuth from "next-auth";
 import Providers from "next-auth/providers";
+
+import { fauna } from "../../../services/fauna";
 
 export default NextAuth({
   // Configure one or more authentication providers
@@ -14,4 +18,12 @@ export default NextAuth({
 
   // A database is optional, but required to persist accounts in a database
   database: process.env.DATABASE_URL,
+
+  callbacks: {
+    async signIn({ user, account, profile }) {
+      const { email } = user;
+      await fauna.query(q.Create(q.Collection("users"), { data: { email } }));
+      return true;
+    },
+  },
 });
