2193695547eff40379fb4010b33fdd2e96e3617f
Luiz Eduardo <luiz.lr.2010@gmail.com>
2021-12-13 15:16:36 -0300

    clear

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -6,20 +6,16 @@ import Providers from "next-auth/providers";
 import { fauna } from "../../../services/fauna";
 
 export default NextAuth({
-  // Configure one or more authentication providers
   providers: [
     Providers.GitHub({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
       scope: "read:user",
     }),
-    // ...add more providers here
   ],
 
   callbacks: {
     async signIn(user, account, profile) {
-      // await fauna.query(q.Create(q.Collection("users"), { data: { email } }));
-      // return true;
       try {
         const { email } = user;
         await fauna.query(q.Create(q.Collection("users"), { data: { email } }));
