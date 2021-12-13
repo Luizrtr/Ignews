8743424f22c9b99701a417ce00a707162086951c
Luiz Eduardo <luiz.lr.2010@gmail.com>
2021-12-13 15:31:22 -0300

    remove jwt

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -6,26 +6,19 @@ import Providers from "next-auth/providers";
 import { fauna } from "../../../services/fauna";
 
 export default NextAuth({
-  // Configure one or more authentication providers
   providers: [
     Providers.GitHub({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
       scope: "read:user",
     }),
-    // ...add more providers here
   ],
-  jwt: {
-    signinKey: process.env.SIGNIN_KEY,
-  },
   callbacks: {
     async signIn(user, account, profile) {
-      // await fauna.query(q.Create(q.Collection("users"), { data: { email } }));
-      // return true;
+      const { email } = user;
+
       try {
-        const { email } = user;
         await fauna.query(q.Create(q.Collection("users"), { data: { email } }));
-        console.log(user);
         return true;
       } catch {
         return false;
