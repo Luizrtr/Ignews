ba341513e9798bd1cb56409b8fa16b80386022d4
Luiz Eduardo <luiz.lr.2010@gmail.com>
2021-12-13 15:28:32 -0300

    Add jwt

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -6,16 +6,22 @@ import Providers from "next-auth/providers";
 import { fauna } from "../../../services/fauna";
 
 export default NextAuth({
+  // Configure one or more authentication providers
   providers: [
     Providers.GitHub({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
       scope: "read:user",
     }),
+    // ...add more providers here
   ],
-
+  jwt: {
+    signinKey: process.env.SIGNIN_KEY,
+  },
   callbacks: {
     async signIn(user, account, profile) {
+      // await fauna.query(q.Create(q.Collection("users"), { data: { email } }));
+      // return true;
       try {
         const { email } = user;
         await fauna.query(q.Create(q.Collection("users"), { data: { email } }));
