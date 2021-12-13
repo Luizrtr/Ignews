53a502be0356b6b7fd92f3c3d8cf4ad148468883
Luiz Eduardo <luiz.lr.2010@gmail.com>
2021-12-13 16:58:04 -0300

    custom checkout stripe

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,12 +1,31 @@
+import { useSession, signIn } from "next-auth/client";
 import styles from "./styles.module.scss";
 
 interface SubscrisbeButtonProps {
   priceId: string;
 }
 
+// Os três lugares que devem ser usadas as credenciais(back-end)
+// getServerSideProps (SSR)
+// getStaticProps (SSG)
+// API routes
+
 export function SubscribeButton({ priceId }: SubscrisbeButtonProps) {
+  const [session] = useSession();
+
+  function handleSubscribe() {
+    if (!session) {
+      signIn("github");
+      return;
+    }
+  }
+
   return (
-    <button type="button" className={styles.subscribeButton}>
+    <button
+      type="button"
+      className={styles.subscribeButton}
+      onClick={handleSubscribe}
+    >
       Subscribe Now
     </button>
   );
