5a9c520fd0f4164ddb9b148d0aa1d6908dc759b6
Luiz Eduardo <luiz.lr.2010@gmail.com>
2021-12-27 10:59:51 -0300

    inserindo os post static

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,11 +1,21 @@
 import { GetStaticProps } from "next";
 import Head from "next/head";
 import Prismic from "@prismicio/client";
-
+import { RichText } from "prismic-dom";
 import { getPrismicClient } from "../../services/prismic";
 import styles from "./styles.module.scss";
 
-export default function Posts() {
+type Post = {
+  slug: string;
+  title: string;
+  excerpt: string;
+  updateAt: string;
+};
+interface PostProps {
+  posts: Post[];
+}
+
+export default function Posts({ posts }: PostProps) {
   return (
     <>
       <Head>
@@ -13,14 +23,13 @@ export default function Posts() {
       </Head>
       <main className={styles.container}>
         <div className={styles.posts}>
-          <a href="#">
-            <time>12 de março de 2021</time>
-            <strong>What is Lorem Ipsum?</strong>
-            <p>
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry.
-            </p>
-          </a>
+          {posts.map((post) => (
+            <a href="#" key={post.slug}>
+              <time>{post.updateAt}</time>
+              <strong>{post.title}</strong>
+              <p>{post.excerpt}</p>
+            </a>
+          ))}
         </div>
       </main>
     </>
@@ -33,12 +42,24 @@ export const getStaticProps: GetStaticProps = async () => {
     [Prismic.predicates.at("document.type", "publication")],
     {
       fetch: ["publication.title", "publication.content"],
-      // pageSize: 100,
+      pageSize: 100,
     }
   );
 
-  console.log(response);
+  const posts = response.results.map((post) => {
+    return {
+      slug: post.uid,
+      title: RichText.asText(post.data.title),
+      excerpt:
+        post.data.content.find((content) => content.type === "paragraph")
+          ?.text ?? "",
+      updateAt: new Date(post.last_publication_date).toLocaleDateString(
+        "pt-br",
+        { day: "2-digit", month: "long", year: "numeric" }
+      ),
+    };
+  });
   return {
-    props: {},
+    props: { posts },
   };
 };
