9759e2c537f604d338fc39c29968b7fc569303a4
Luiz Eduardo <luiz.lr.2010@gmail.com>
2022-01-03 09:43:34 -0300

    Preview posts

--- src/pages/posts/preview/[slug].tsx
+++ src/pages/posts/preview/[slug].tsx
@@ -1,8 +1,10 @@
 import { GetStaticProps } from "next";
-import { getSession } from "next-auth/client";
+import { useSession } from "next-auth/client";
 import Head from "next/head";
 import Link from "next/link";
+import { useRouter } from "next/router";
 import { RichText } from "prismic-dom";
+import { useEffect } from "react";
 import { getPrismicClient } from "../../../services/prismic";
 
 import styles from "../post.module.scss";
@@ -16,6 +18,13 @@ interface PostPreviewProps {
 }
 
 export default function PostPreview({ post }: PostPreviewProps) {
+  const [session] = useSession();
+  const router = useRouter();
+  useEffect(() => {
+    if (session?.activeSubscription) {
+      router.push(`/posts/${post.slug}`);
+    }
+  }, [session]);
   return (
     <>
       <Head>
