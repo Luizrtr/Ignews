d511348a8d32f2f3cef0b242283e90a013be871c
Luiz Eduardo <luiz.lr.2010@gmail.com>
2022-01-03 10:28:52 -0300

    Finalizacao!

--- src/pages/posts/preview/[slug].tsx
+++ src/pages/posts/preview/[slug].tsx
@@ -1,4 +1,4 @@
-import { GetStaticProps } from "next";
+import { GetStaticProps, GetStaticPaths } from "next";
 import { useSession } from "next-auth/client";
 import Head from "next/head";
 import Link from "next/link";
@@ -51,7 +51,7 @@ export default function PostPreview({ post }: PostPreviewProps) {
   );
 }
 
-export const getStaticPaths = () => {
+export const getStaticPaths: GetStaticPaths = async () => {
   return {
     paths: [],
     fallback: "blocking",
@@ -82,5 +82,6 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
     props: {
       post,
     },
+    redirect: 60 * 30,
   };
 };
