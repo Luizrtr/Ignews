1d908c0250589a7ae0f3b0e95484085bee18f907
Luiz Eduardo <luiz.lr.2010@gmail.com>
2022-01-07 09:09:46 -0300

    tests getStaticProps

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { GetServerSideProps } from "next";
+import { GetStaticProps } from "next";
 import Head from "next/head";
 import styles from "./home.module.scss";
 import { SubscribeButton } from "../components/SubscribeButton";
@@ -35,7 +35,7 @@ export default function Home({ product }: HomeProps) {
   );
 }
 
-export const getStaticProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const price = await stripe.prices.retrieve("price_1K7L8wLEYV1YF9jA722fsysf");
 
   const product = {
