a246200800d50809388b2d89ed300c2f1fc954f9
Luiz Eduardo <luiz.lr.2010@gmail.com>
2022-01-07 08:48:44 -0300

    Tests Home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,7 +7,7 @@ import { stripe } from "../services/stripe";
 interface HomeProps {
   product: {
     priceId: string;
-    amount: number;
+    amount: string;
   };
 }
 
@@ -27,7 +27,7 @@ export default function Home({ product }: HomeProps) {
             Get acces to all the publications <br />
             <span>for {product.amount} month</span>
           </p>
-          <SubscribeButton priceId={product.priceId} />
+          <SubscribeButton />
         </section>
         <img src="/images/avatar.svg" alt="Girl coding" />
       </main>
