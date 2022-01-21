230cc59fc5359c38314e0670fc59ac61ace910fc
Luiz Eduardo <luiz.lr.2010@gmail.com>
2022-01-21 09:16:05 -0300

    verif coverage

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,20 @@
 module.exports = {
-    testPathIgnorePatterns: ["/node-modules/", "/.next/"],
-    setupFilesAfterEnv: [
-        "<rootDir>/src/tests/setupTests.ts"
-      ],
-      transform: {
-      "^.+\\.(js|jsx|ts|tsx)$": "<rootDir>/node_modules/babel-jest"
-    },
-    testEnvironment: 'jsdom',
-    moduleNameMapper: {
-      "\\.(scss|css|sass)$": 'identity-obj-proxy'
-    },
-  };
+  testPathIgnorePatterns: ["/node-modules/", "/.next/"],
+  setupFilesAfterEnv: ["<rootDir>/src/tests/setupTests.ts"],
+  transform: {
+    "^.+\\.(js|jsx|ts|tsx)$": "<rootDir>/node_modules/babel-jest",
+  },
+  testEnvironment: "jsdom",
+  moduleNameMapper: {
+    "\\.(scss|css|sass)$": "identity-obj-proxy",
+  },
+  collectCoverage: true,
+  collectCoverageFrom: [
+    "src/**/*.tsx",
+    "!src/**/*.spec.{tsx}",
+    "!src/**/_app.tsx",
+    "!src/**/_document.tsx",
+  ],
+  coverageReporters: ["json", "lcov"],
+  // testEnvironment: 'jsdom',
+};
